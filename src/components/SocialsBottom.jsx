70c284bfc16b5b5176ff08effec02a07001cc28f
Jourvence <117796287+Vinqueire@users.noreply.github.com>
2024-11-13 22:32:26 +0100

    I need to make the email and linkedIn buttons work

--- src/components/SocialsBottom.jsx
+++ src/components/SocialsBottom.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import twt from '../images/twt.png';
+import fac from '../images/fac.png';
+import insta from '../images/insta.png';
+import github from '../images/github.png';
 
 function SocialsBottom() {
     return (
         <>
            <div id="socialsBottomContainer">
-                <p>XDDD</p>
+                <a href="https://x.com/"><img src={twt} alt="twitter" id="socialImgs" /></a>
+                <a href="https://www.facebook.com"><img src={fac} alt="fac" id="socialImgs" /></a>
+                <a href="https://www.instagram.com"><img src={insta} alt="insta" id="insta" /></a>
+                <a href="https://github.com/Jourvence"><img src={github} alt="github" id="github"/></a>
            </div>
         </>
     )
