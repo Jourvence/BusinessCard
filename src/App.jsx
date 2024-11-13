7b1aaa660a13841bef2faf51ed03949da00dccc8
Jourvence <117796287+Vinqueire@users.noreply.github.com>
2024-11-13 18:31:17 +0100

    I made a card containter that adapts to the screen wiith with grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-import TopImg from './components/TopImg'
-import SingleCard from './components/SingleCard'
-
+import CardContainer from './components/CardContainer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <SingleCard/>
-      
+      <CardContainer/>
+     
     </>
   )
 }
---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import SingleCard from "./SingleCard";
+
+
+function CardContainer() {
+    return (
+        <>
+            <div id="cardContainer">
+                <SingleCard/>
+                <SingleCard/>
+                <SingleCard/>
+                <SingleCard/>
+                <SingleCard/>
+                <SingleCard/>
+                <SingleCard/>
+                <SingleCard/>
+                <SingleCard/>
+                <SingleCard/>
+            </div>
+        </>
+    )
+}
+
+export default CardContainer
